22aa1489284ca36f6e19d347c3cae6c57adb05ff
marcondercin <marcondercin@gmail.com>
2021-08-28 09:06:53 -0700

    Refactor declared variables to loop

--- new_style/script.js
+++ new_style/script.js
@@ -1,16 +1,16 @@
-let arrow1 = document.getElementById('arrow1');
-let arrow2 = document.getElementById('arrow2');
-let arrow3 = document.getElementById('arrow3');
-let arrow4 = document.getElementById('arrow4');
-let project1 = document.getElementById('project-1');
-let project2 = document.getElementById('project-2');
-let project3 = document.getElementById('project-3');
-let project4 = document.getElementById('project-4');
 
-project1.addEventListener('click', () => {displayProject(arrow1, '#project-1-content')});
-project2.addEventListener('click', () => {displayProject(arrow2, '#project-2-content')});
-project3.addEventListener('click', () => {displayProject(arrow3, '#project-3-content')});
-project4.addEventListener('click', () => {displayProject(arrow4, '#project-4-content')});
+// Loop through project list, as long as each project has the proper html attributes.
+let projects = document.querySelectorAll('#projects > h3');
+
+for (let i = 0; i < projects.length; i++) {
+    let arrow = 'arrow' + (i+1).toString();
+    projects[i].addEventListener('click', () => {
+        displayProject(
+            document.getElementById(arrow),
+            '#' + projects[i].id + '-content'
+            )
+    })
+}
 
 function displayProject(arrow, id) {
     if (arrow.textContent === '>') {
